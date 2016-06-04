80600c90523f6bae9c51197503d233124a500858
Staszek Welsh <stalefishies@gmail.com>
2016-06-04 21:05:01 +0100

    Added comments to rect implementation

--- src/gui_base.cpp
+++ src/gui_base.cpp
@@ -19,14 +19,17 @@ namespace gui {
     double rect::centreX() const { return (left + right) / 2.0; }
     double rect::centreY() const { return (top + bottom) / 2.0; }
     
+    // set rect position by left, right, top, bottom
     void rect::setLRTB(double _left, double _right, double _top, double _bottom) {
         left = _left; right = _right; top = _top; bottom = _bottom;
     }
     
+    // set rect position by x, y, width, height
     void rect::setXYWH(double x, double y, double width, double height) {
         left = x; right = x + width; top = y; bottom = y + height;
     }
     
+    // get x and y coordinates of a Position within the rect
     coord rect::getPos(Position position) const {
         coord ret;
         
@@ -46,12 +49,14 @@ namespace gui {
         return ret;
     }
     
+    // move the rect so that a Position within the rect is now at pos
     void rect::movePos(Position position, coord pos) {
         double x = pos.x;
         double y = pos.y;
         double W = width();
         double H = height();
         
+        // set location by x and y of top-left corner, preserving width and height
         switch(position) {
             case TOP_LEFT:     { setXYWH(x,           y,           W, H); } break;
             case TOP:          { setXYWH(x - W / 2.0, y,           W, H); } break;
@@ -66,6 +71,7 @@ namespace gui {
         }
     }
     
+    // move rect by an offset
     void rect::moveBy(coord offset) {
         left   += offset.x;
         right  += offset.x;
@@ -73,6 +79,7 @@ namespace gui {
         bottom += offset.y;
     }
     
+    // return true if the given coordinates lie inside or on the boundary of the rectangle
     bool rect::inside(double x, double y) const {
         if (x >= left && x <= right && y >= top && y <= bottom) {
             return true;
